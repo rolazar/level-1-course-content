4f023253ff73c9b4d40f37a2d05695756fb43d36
ReeceRenninger <renningerreece@gmail.com>
2023-10-17 19:46:37 -0700

    week 4 day one assignment completed with sample code created

---
+++ week-4/day-1/fundamentals.js
@@ -0,0 +1,22 @@
+'use strict';
+
+let firstName = 'John';
+
+let lastName = 'Doe';
+
+let age = 35;
+
+console.log(firstName + ' ' + lastName + ' is ' + age + ' years old.');
+
+const favoriteFood = 'Pizza';
+
+const likesPizza = true;
+
+console.log(firstName + ' likes ' + favoriteFood + '.');
+console.log('Whenever anyone asks me if I like pizza, I always say ' + likesPizza + '.');
+
+let favoriteMovie = 'Interstellar';
+
+const likesRomanceMovies = false;
+
+console.log(firstName + ' likes ' + favoriteMovie + '.' + ' However, whenever anyone asks me if I like romance movies, I always say ' + likesRomanceMovies + '.');
