53ec49b3be6e3d8dc937426ced7211938acf5027
Robert McCreary Mannino <fooyinfighter@gmail.com>
2023-11-19 14:40:45 -0800

    add telephone game code

---
+++ week-6/day-2/Rm-class-materials/day-2-starter-code/messages.js
@@ -0,0 +1,39 @@
+// Please don't modify this file.
+
+let names;
+
+names = [
+  "Alice",
+  "Bob",
+  "Carol",
+  "Dan",
+  "Ellie",
+  "Frank",
+  "Greg",
+  "Harry",
+  "Ivy",
+  "Jack"
+];
+
+function setNames () {
+  for(let name of names) {
+    let lowerName, divId, propertyName, message, elm;
+    // make name lowercase
+    lowerName = name.toLowerCase();
+    // the id of the div is 'name-says'
+    divId = `${lowerName}-says`;
+    // the name of the variable is 'nameSays'
+    propertyName = `${lowerName}Says`;
+    // The message is in the variable
+    // which was set in script.js
+    // In JS we can access it with the magic of 'this'
+    message = this[propertyName];
+    if(!message) {
+      message = 'undefined';
+    } 
+    elm = document.getElementById(divId);
+    elm.textContent = message;
+  }
+}
+
+setNames();
---
+++ week-6/day-2/Rm-class-materials/day-2-starter-code/script.js
@@ -0,0 +1,12 @@
+
+// It's a good idea to declare all your variables.
+// This will let our JS linter help us find 
+// spelling errors among other things.
+// Declaring with 'var' will make them global
+// variables.
+var aliceSays, bobSays, CarolSays, danSays, ellieSays, frankSays, harrySays, ivySays, jackSays;
+
+
+aliceSays = "Kathy loves Larry";
+bobSays = aliceSays;
+
